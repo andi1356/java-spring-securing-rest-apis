457d5e0bceecc93ccff95e8fe827accea6e033e2
AnRobu <andreiantonio.robu@endava.com>
2022-09-05 09:10:11 +0300

    partial commit - controller

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -1,12 +1,49 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserRepositoryUserDetailsService implements UserDetailsService {
+    private final UserRepository users;
+
+    public UserRepositoryUserDetailsService(UserRepository users) {
+        this.users = users;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        throw new UsernameNotFoundException("no user");
+        return users.findByUsername(s)
+                .map(BridgeUser::new)
+                .orElseThrow(() -> new UsernameNotFoundException("invalid user"));
+    }
+
+    private static class BridgeUser extends User implements UserDetails {
+        public BridgeUser(User user) {
+            super(user);
+        }
+        public List<GrantedAuthority> getAuthorities() {
+            return this.userAuthorities.stream()
+                    .map(UserAuthority::getAuthority)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        }
+
+        public boolean isAccountNonExpired() {
+            return this.enabled;
+        }
+
+        public boolean isAccountNonLocked() {
+            return this.enabled;
+        }
+
+        public boolean isCredentialsNonExpired() {
+            return this.enabled;
+        }
     }
 }
