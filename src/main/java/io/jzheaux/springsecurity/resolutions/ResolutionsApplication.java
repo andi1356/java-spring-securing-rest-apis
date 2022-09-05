c068300570ce36c3c2f25327c054799e7a3623bc
AnRobu <andreiantonio.robu@endava.com>
2022-09-05 09:41:40 +0300

    taskFULL hopefully

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -5,14 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import static org.springframework.http.HttpMethod.GET;
 
 import javax.sql.DataSource;
-import java.util.List;
 
 @SpringBootApplication
 public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
@@ -21,25 +18,10 @@ public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
 		SpringApplication.run(ResolutionsApplication.class, args);
 	}
 
-
 	@Bean
-	UserDetailsService userDetailsService(DataSource dataSource) {
-		return new JdbcUserDetailsManager(dataSource) {
-			@Override
-			protected List<GrantedAuthority> loadUserAuthorities(String username) {
-				return AuthorityUtils.createAuthorityList("resolution:read");
-			}
-		};
+	UserDetailsService userDetailsService(UserRepository users) {
+		return new UserRepositoryUserDetailsService(users);
 	}
-//	@Bean
-//	public UserDetailsService userDetailsService() {
-//		return new InMemoryUserDetailsManager(
-//				org.springframework.security.core.userdetails.User
-//						.withUsername("user")
-//						.password("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W")
-//						.authorities("resolution:read")
-//						.build());
-//	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
