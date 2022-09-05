2a95b78f0c6f0983e6800210aef5a2791235dfb0
AnRobu <andreiantonio.robu@endava.com>
2022-09-05 09:38:42 +0300

    task6

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import static org.springframework.http.HttpMethod.GET;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @SpringBootApplication
 public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
@@ -19,15 +21,25 @@ public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
 		SpringApplication.run(ResolutionsApplication.class, args);
 	}
 
+
 	@Bean
-	public UserDetailsService userDetailsService() {
-		return new InMemoryUserDetailsManager(
-				org.springframework.security.core.userdetails.User
-						.withUsername("user")
-						.password("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W")
-						.authorities("resolution:read")
-						.build());
+	UserDetailsService userDetailsService(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource) {
+			@Override
+			protected List<GrantedAuthority> loadUserAuthorities(String username) {
+				return AuthorityUtils.createAuthorityList("resolution:read");
+			}
+		};
 	}
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		return new InMemoryUserDetailsManager(
+//				org.springframework.security.core.userdetails.User
+//						.withUsername("user")
+//						.password("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W")
+//						.authorities("resolution:read")
+//						.build());
+//	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
